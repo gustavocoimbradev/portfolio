0c1dafcf9b3aa389276df4c67587dabe49a61037
Gustavo Coimbra <whoisgustavolima@gmail.com>
2023-05-01 15:43:38 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
+import { useEffect } from 'react'
+
+import '@/styles/template.scss'
 import type { AppProps } from 'next/app'
 
+const loadBootstrap = () => {
+  if (typeof window !== 'undefined') {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    loadBootstrap();
+  }, [])
+
   return <Component {...pageProps} />
 }
